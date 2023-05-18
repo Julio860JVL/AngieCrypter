50d4872d0f704d3d330d6a761e9153efe4ff09bb
Julio860JVL <Julio860JVL+GitHub@outlook.com>
2023-05-18 19:40:02 -0400

    Refactoring: Removing comments on how long it took me to complete this project. I did it in 10 hours.

--- Angie/Program.cs
+++ Angie/Program.cs
@@ -1,14 +1,5 @@
 ﻿using AngieCrypter;
 
-// started this project at 1:15 PM.
-// Took a 1 hours and 45 min brake frome 5:15 PM to 7 PM.
-// Total of 4 hours so far in this project.
-// Finished at 8 PM.
-// Total of 5 hours.
-// I noticed at 8 PM the symbols were coded wrong. So I had to continue.
-// The time now is almost 10:30 PM.
-// Total of 7 hours and 30 minutes coding this project.
-
 while (true)
 {
     // If the method 'Crypto.EncryptDecrypt()' is called then it will be called twice.
