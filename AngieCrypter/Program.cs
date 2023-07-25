e6c0567f7e1553bca7a1a6ea83046f20569ed7a1
Julio860JVL <Julio860JVL+GitHub@outlook.com>
2023-07-25 01:59:30 -0400

    GitHub conflict test_v3.

--- AngieCrypter/Program.cs
+++ AngieCrypter/Program.cs
@@ -6,5 +6,6 @@ while (true)
     // This first line of code also calls the method 'Crypto.EncryptDecrypt()'.
     string userInput = Crypto.EncryptDecrypt();
 
+    // GitHub conflict test_v3.
     Validate.Input(userInput);
 }
